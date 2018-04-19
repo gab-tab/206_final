9097c3ffb81d49f806a5aec496bbc9d2d9cbb6df
Gaby Tabachnik <gtabach@umich.edu>
2018-04-18 20:18:18 -0400

    updates

--- process_data.py
+++
@@ -1,9 +0,0 @@
-f = open('data.txt')
-names = ''
-for line in f:
-    line = line.strip().split()
-    col_name = line[0]
-    col_name = col_name[1:-1]
-    print("    " + col_name + ' = info["' + col_name + '"]')
-    names = names + ',' + col_name
-print(names)
--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 import unittest
 import json
 import final_project as project
-from final_project import *
+from ok import *
 
 class TestDatabase(unittest.TestCase):
     def test_city(self):
@@ -46,16 +46,47 @@ class TestTweets(unittest.TestCase):
         self.assertTrue(len(tweets1) > 0 and len(tweets1) < 10)
 
         tweets2 = get_tweets_for_rest(rest2)
-        self.assertEqual(len(tweets2), 10) # there have to be at least 10 for Yellowstone!
+        self.assertEqual(len(tweets2), 10)
 
         self.assertFalse(tweets2[2].is_retweet)
         self.assertTrue(tweets2[0].popularity_score >= tweets2[1].popularity_score)
         self.assertTrue(tweets2[3].popularity_score >= tweets2[7].popularity_score)
 
-        tweet_string = str(tweets2[0]) # will call Tweet.__str__()
+        tweet_string = str(tweets2[0])
         self.assertTrue('@' in tweet_string)
         self.assertTrue('retweeted' in tweet_string)
         self.assertTrue('favorited' in tweet_string)
+
+class TestPlotly(unittest.TestCase):
+    def test_pie(self):
+        sql = "SELECT price_level, count(1) as count FROM ( "
+        sql += "SELECT pricerangemax, case  "
+        sql += "when pricerangemax < 15 then 'Very Cheap' "
+        sql += "when pricerangemax < 30 then 'Cheap' "
+        sql += "when pricerangemax < 50 or pricerangemax is NULL or pricerangemax = '' then 'Average' "
+        sql += "when pricerangemax < 100 then 'Expensive' "
+        sql += "else 'Very Expensive'  end as price_level "
+        sql += "FROM Pizza) tmp group by price_level"
+
+        cur.execute(sql)
+        lst = cur.fetchall()
+        self.assertEqual(len(lst),5)
+
+    def test_ratings(self):
+        user_city = 'Los Angeles'
+        city_id = '2'
+        sql = "SELECT ID FROM City WHERE name = ?;"
+        cur.execute(sql, [user_city])
+        city_id = cur.fetchall()
+        city_id = city_id[0]
+        city_id = city_id[0]
+        sql = "SELECT DISTINCT name FROM Pizza WHERE city = ?;"
+        cur.execute(sql, [city_id])
+        lst = cur.fetchall()
+        cities = []
+        for item in lst:
+            cities.append(item[0])
+        self.assertEqual(len(cities),8)
 
 
 
